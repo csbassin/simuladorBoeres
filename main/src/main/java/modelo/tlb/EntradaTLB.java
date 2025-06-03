7ebed96917bae83ab46a63314e2eabf13ff8e561
Lucas Avelar <77032122+1lit4@users.noreply.github.com>
2025-06-02 22:59:42 -0300

    Implementação inicial TLB (incompleta)
    Co-authored-by: Bernardo_M <mendes_bernardo@id.uff.br>

--- main/src/main/java/modelo/tlb/EntradaTLB.java
+++ main/src/main/java/modelo/tlb/EntradaTLB.java
@@ -3,8 +3,63 @@ package modelo.tlb;
 import modelo.tabelaPaginas.EntradaTP;
 
 public class EntradaTLB {
-	private byte valid;	// quando houver uma troca de processo, o bit de validade deve ser setado para 1
-    private int numPagina; 
+	private boolean valid;	// quando houver uma troca de processo, o bit de validade deve ser setado para 1
+    private int numPagina;
+
+    //Usaremos LRU, em que o tempo de ultimo uso é um contador
     private int tempoUltimoUso; // essa tem relação com a política de susbtituição da TLB, e não da tabela de páginas
     private EntradaTP entradaTP;
+
+    private boolean modificacao;
+    private boolean presenca;
+    private int numQuadro;
+
+    public EntradaTLB(int numPagina, EntradaTP entradaTP) {
+        this.numPagina = numPagina;
+        this.entradaTP = entradaTP;
+
+        this.tempoUltimoUso = 0;
+        modificacao = entradaTP.getModificacao();
+        presenca = entradaTP.getPresenca();
+        numQuadro = entradaTP.getNumQuadro();
+        valid = true;
+    }
+
+    public int getNumPagina() {
+        return numPagina;
+    }
+
+    public boolean isPresenca() {
+        return presenca;
+    }
+
+    public int getNumQuadro() {
+        return numQuadro;
+    }
+
+    public int getTempoUltimoUso() {
+        return tempoUltimoUso;
+    }
+
+    public void setTempoUltimoUso(int tempoUltimoUso) {
+        this.tempoUltimoUso = tempoUltimoUso;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public boolean isModificacao() {
+        return modificacao;
+    }
+
+    public void setModificacao(boolean modificacao) {
+        this.modificacao = modificacao;
+    }
+
+
 }
--- main/src/main/java/modelo/tlb/TLB.java
+++ main/src/main/java/modelo/tlb/TLB.java
@@ -1,5 +1,33 @@
 package modelo.tlb;
 
+import java.util.ArrayList;
+
 public class TLB {
+    private int qtdPaginas;
+    private ArrayList<EntradaTLB> entradas;
+
+
+    public TLB(int qtdPaginas) {
+        this.qtdPaginas = qtdPaginas;
+        entradas = new ArrayList<>(qtdPaginas);
+    }
+
+    private void substituicaoLRU(EntradaTLB entradaTLB){
+        int maior_indice = 0;
+        for (int i = 0; i<qtdPaginas; i++) {
+            if(entradas.get(i).getTempoUltimoUso() > maior_indice)
+                maior_indice = i;
+        }
+        entradas.set(maior_indice, entradaTLB);
+    }
+
+    public void adicionarEntrada(EntradaTLB entradaTLB) {
+        if (entradas.size() < qtdPaginas)
+            entradas.add(entradaTLB);
+        else
+            substituicaoLRU(entradaTLB);
+    }
+
+
 
 }
